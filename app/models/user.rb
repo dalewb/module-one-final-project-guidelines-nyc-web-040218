5273d2608e79743cb50041018ccf504942924d9d
William Dale <williambdale@gmail.com>
2018-04-19 13:46:09 -0400

    User changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ActiveRecord::Base
   has_many :companies, through: :transactions
 
   def find_num_shares(company)
-  return_company = self.transactions.find do |trans|
-    company.id == trans.company_id
+    return_company = self.transactions.select do |trans|
+      company.id == trans.company_id
     end
     return_company.num_of_shares
   end
+
 end
